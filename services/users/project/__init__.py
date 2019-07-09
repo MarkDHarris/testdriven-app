02c6c356935b564e62a1ca90a06a7cabb6aa8065
Mark Harris <harriskp@amazon.com>
2019-07-09 05:32:30 -0400

    Adding postgres setup | Part 1 Chapter 7

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,6 +4,7 @@ import os
 import sys
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -14,6 +15,19 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
+db = SQLAlchemy(app)
+
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 class UsersPing(Resource):
     def get(self):
         return {
@@ -21,6 +35,6 @@ class UsersPing(Resource):
         'message': 'pong!'
     }
 
-print(app.config, file=sys.stderr)
+# print(app.config, file=sys.stderr)
 
 api.add_resource(UsersPing, '/users/ping')
